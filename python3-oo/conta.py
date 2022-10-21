9a266b499ccec474f451d646acadf3160a23eb5c
GuiFerreira11 <guifl_pereira@hotmail.com>
2022-10-20 21:43:18 -0300

    Fim da parte 3 do curso de IOO

--- python3-oo/conta.py
+++ python3-oo/conta.py
@@ -6,3 +6,12 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print("O saldo do titular {} é R${:.2f}".format(self.titular, self.saldo))
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
