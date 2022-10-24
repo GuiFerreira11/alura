3547823b1a7502f1323f0d8e1ac92ae597df8f51
GuiFerreira11 <guifl_pereira@hotmail.com>
2022-10-23 21:12:26 -0300

    Fim da parte 4 do curso de IOO

--- python3-oo/conta.py
+++ python3-oo/conta.py
@@ -2,16 +2,20 @@ class Conta:
     def __init__(self, numero, titular, saldo, limite):
 
         print("Construindo um objeto ...")
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("O saldo do titular {} é R${:.2f}".format(self.titular, self.saldo))
+        print("O saldo do titular {} é R${:.2f}".format(self.__titular, self.__saldo))
 
-    def deposita(self, valor):
-        self.saldo += valor
+    def depositar(self, valor):
+        self.__saldo += valor
 
-    def saca(self, valor):
-        self.saldo -= valor
+    def sacar(self, valor):
+        self.__saldo -= valor
+
+    def transferir(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
