eb0aff4a0c1285f4d7b81f2ab46d5daf5563e400
GuiFerreira11 <guifl_pereira@hotmail.com>
2022-10-23 21:47:05 -0300

    Fim da parte 5 do curso de IOO

--- python3-oo/conta.py
+++ python3-oo/conta.py
@@ -19,3 +19,23 @@ class Conta:
     def transferir(self, valor, destino):
         self.sacar(valor)
         destino.depositar(valor)
+
+    @property
+    def numero(self):
+        return self.__numero
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, novo_limite):
+        self.__limite = novo_limite
