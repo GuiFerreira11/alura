0151bc9666bd398f18997b06b638d901fce1fce1
Gui Ferreira <guifl_pereira@hotmail.com>
2022-11-26 22:58:05 -0300

    Fim da parte 2 do curso python-strings

--- python3-strings/main.py
+++ python3-strings/main.py
@@ -6,7 +6,21 @@ indice_interrogacao = url.find("?")
 url_base = url[:indice_interrogacao]
 print(url_base)
 
-url_parametro = url[: indice_interrogacao + 1 :]
+url_parametro = url[indice_interrogacao + 1 :]
 print(url_parametro)
 
 print(url)
+
+# parametro_busca = "quantidade"
+parametro_busca = "moedaDestino"
+# parametro_busca = "moedaOrigem"
+indice_parametro = url_parametro.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametro.find("&", indice_valor)
+
+if indice_e_comercial == -1:
+    valor = url_parametro[indice_valor:]
+else:
+    valor = url_parametro[indice_valor:indice_e_comercial]
+
+print(valor)
