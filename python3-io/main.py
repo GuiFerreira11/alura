44fbf19ff67259d24bff1c32cdd72178314fd4d9
Gui Ferreira <guifl_pereira@hotmail.com>
2023-01-16 19:19:02 -0300

    Fim da parte 2 do curso de Python I/O

--- python3-io/main.py
+++ python3-io/main.py
@@ -1,4 +1,16 @@
-f = open("./dados/contatos.csv", encoding="latin_1")
+f = open("./dados/contatos-novo.csv", encoding="latin_1", mode="r+")
 
-for l in f:
-    print(l, end="")
+contatos = [
+    "11,Carol,carol@carol.com.br\n",
+    "12,Ana,ana@ana.com.br\n",
+    "13,Tais,tais@tais.com.br\n",
+    "14,Felipe,felipe@felipe.com.br\n",
+]
+
+for contato in contatos:
+    f.write(contato)
+
+# f.seek(26)
+
+# f.write("Teste\n")
+# f.write("Teste\n")
