f658944df6d2db38e9aa3eaf95f2be2f1424b7aa
Gui Ferreira <guifl_pereira@hotmail.com>
2023-01-17 17:18:31 -0300

    Fim da parte 3 do curso de Python I/O

--- python3-io/main.py
+++ python3-io/main.py
@@ -1,16 +1,8 @@
-f = open("./dados/contatos-novo.csv", encoding="latin_1", mode="r+")
-
-contatos = [
-    "11,Carol,carol@carol.com.br\n",
-    "12,Ana,ana@ana.com.br\n",
-    "13,Tais,tais@tais.com.br\n",
-    "14,Felipe,felipe@felipe.com.br\n",
-]
-
-for contato in contatos:
-    f.write(contato)
-
-# f.seek(26)
-
-# f.write("Teste\n")
-# f.write("Teste\n")
+try:
+    with open("./dados/contatos-novo.csv", encoding="latin_1", mode="r+") as file:
+        for line in file:
+            print(line, end="")
+except FileNotFoundError:
+    print("Arquivo não encontrado")
+except PermissionError:
+    print("Permissão de acesso negada")
