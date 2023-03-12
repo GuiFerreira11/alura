f86b82f11380c0085fccffdd53762e2f212eb8be
Gui Ferreira <guifl_pereira@hotmail.com>
2023-03-12 19:43:36 -0300

    Fim da parte 2 do curso django-formularios-requisicoes

--- django-formularios-requisicoes/usuarios/views.py
+++ django-formularios-requisicoes/usuarios/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 
 def cadastro(request):
-    return render(request, "usuarios/cadastro.html")
+    if request.method == "POST":
+        nome = request.POST["nome"]
+        email = request.POST["email"]
+        senha1 = request.POST["password"]
+        senha2 = request.POST["password2"]
+        if not nome.strip():
+            print("O campo 'nome' está em branco")
+            return render(request, "usuarios/cadastro.html")
+        if senha1 != senha2:
+            print("As senhas são diferentes")
+            return render(request, "usuarios/cadastro.html")
+        if User.objects.filter(email=email).exists():
+            print("Usuário já cadastrado")
+            return render(request, "usuarios/cadastro.html")
+        user = User.objects.create(username=nome, email=email, password=senha1)
+        user.save()
+        print("Usuário criado com sucesso!")
+        return redirect("login")
+    else:
+        return render(request, "usuarios/cadastro.html")
 
 
 def login(request):
