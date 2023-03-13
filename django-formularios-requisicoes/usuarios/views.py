2454ed54e50d8bd3d4c83e0b48f8ac9a6d2b025c
Gui Ferreira <guifl_pereira@hotmail.com>
2023-03-13 17:19:22 -0300

    Fim da parte 3 do curso django-formularios-requisicoes

--- django-formularios-requisicoes/usuarios/views.py
+++ django-formularios-requisicoes/usuarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 
 def cadastro(request):
@@ -17,7 +18,8 @@ def cadastro(request):
         if User.objects.filter(email=email).exists():
             print("Usuário já cadastrado")
             return render(request, "usuarios/cadastro.html")
-        user = User.objects.create(username=nome, email=email, password=senha1)
+        user = User.objects.create(username=nome, email=email)
+        user.set_password(senha1)
         user.save()
         print("Usuário criado com sucesso!")
         return redirect("login")
@@ -26,12 +28,37 @@ def cadastro(request):
 
 
 def login(request):
-    return render(request, "usuarios/login.html")
+    if request.method == "POST":
+        email = request.POST["email"]
+        senha = request.POST["senha"]
+        if not email.strip() or not senha.strip():
+            print("Os campos de e-mail e senha não podem ficar em branco")
+            return render(request, "usuarios/login.html")
+        if User.objects.filter(email=email).exists():
+            username = (
+                User.objects.filter(email=email)
+                .values_list("username", flat=True)
+                .get()
+            )
+            user = auth.authenticate(request, username=username, password=senha)
+            if user is not None:
+                auth.login(request, user)
+                print("Login realizado com sucesso")
+                return redirect("dashboard")
+        else:
+            print("Usuário não cadastrado")
+            return render(request, "usuarios/login.html")
+    else:
+        return render(request, "usuarios/login.html")
 
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect("index")
 
 
 def dashboard(request):
-    pass
+    if request.user.is_authenticated:
+        return render(request, "usuarios/dashboard.html")
+    else:
+        return redirect("index")
